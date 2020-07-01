1886c6045b06e5f5293555be590c67d175ec1719
Jordan Byrd <jabyrd3@gmail.com>
2020-06-30 22:14:02 -0400

    full-text tweets

--- brain.mjs
+++ brain.mjs
@@ -1,24 +1,26 @@
-(f, t, m) => {
-const params = {screen_name: 'trlbo1'};
-  const getTweet = (id) => {
+const params = {
+  screen_name: 'trlbo',
+  tweet_mode: 'extended'
+};
+const getTweet = (id, thus) => {
   console.log(id);
-    return new Promise((res,rej) => {
-      this.tClient
-        .get(`/statuses/show/${id}`, params)
-        .then((t) => {
-          t && console.log('tweet', t.text);
-          this.last = t;
-          // todo: cache id here for reply with @handle
-          res(`@${t.user.screen_name}: ${t.text.replace('\n\n', '\n')}`);
-        })
-        .catch(e => console.log(e));
-    });
-  };
-// put a twit
-const twit = (m) => {
+  return new Promise((res) => {
+    thus.tClient
+      .get(`/statuses/show/${id}`, params)
+      .then((t) => {
+        t && console.log('tweet', t.full_text);
+        thus.last = t;
+        // todo: cache id here for reply with @handle
+        res(`@${t.user.screen_name}: ${t.full_text.replace('\n\n', '\n')}`);
+      })
+      .catch(e => console.log(e));
+  });
+};
+  // put a twit
+const twit = (m, thus) => {
   console.log('twurting', m);
   return new Promise((res, rej) => {
-    this.this.irc.post('statuses/update', {status: m}, (e, t, r) => {
+    thus.irc.post('statuses/update', {status: m}, (e, t, r) => {
       console.log(e);
       if (e) {
         console.log(e);
@@ -29,11 +31,12 @@ const twit = (m) => {
   });
 };
 
+(f, t, m) => {
   console.log('message', f, t, m);
   m.indexOf('https://twitter.com') > -1 &&
   config.ignore.indexOf(f) === -1 &&
   m.indexOf('status') > -1 &&
-    getTweet(m.slice(m.indexOf('status/') + 7, m.indexOf('?') !== -1 ? m.indexOf('?') : m.length))
+    getTweet(m.slice(m.indexOf('status/') + 7, m.indexOf('?') !== -1 ? m.indexOf('?') : m.length), this)
       .then(m => {
         // look for links
         var compiled = m.slice();
@@ -64,12 +67,12 @@ const twit = (m) => {
   config.ignore.indexOf(f) === -1 &&
   f.indexOf(config.ircn) === -1 &&
   f.indexOf(config.sn) === -1 &&
-    twit(m.slice(5))
+    twit(m.slice(5), this)
       .then(() => console.log(config.ircn))
       .catch(e => this.irc.say(config.chan, e[0].message || e));
 
   if (m.indexOf('trlbo blurt on') > -1){
-    console.log('turning blurt off');
+    console.log('turning blurt on');
     config.blurt = true;
   }
   if (m.indexOf('trlbo blurt off') > -1){
